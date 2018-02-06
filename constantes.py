eb0dfe2a9b115684c96f5bf9ca3d3ba811eb1313
Barré <fabien.barre@algosecure.fr>
2018-02-06 17:34:27 +0100

    constantes.py commenter

--- constantes.py
+++ constantes.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*
+#taille d'un des côté du carré d'un sprite sur la grille de jeu
 taille_sprite = 30
+#Nombre de case sur une ligne et colonne de la carte
 taille_carte = 15
+#pixels de début de l'affichage de la grille de jeu
 x_grille = 0
 y_grille = 0
+#calculs de la largeur et hauteur de la grille de jeu
 width_grille = taille_carte*taille_sprite
 height_grille = taille_carte*taille_sprite
 
+#pixels de début de l'affichage de la grille des sorts disponibles
 x_sorts = 0
 y_sorts = y_grille + height_grille
+#calculs de la largeur et hauteur de la grille des sorts disponibles
 height_sorts = 200
 width_sorts = width_grille
 
+#Calcul de la taille de la fenetre
 width_fenetre = width_sorts
 height_fenetre = height_grille+height_sorts
 
@@ -22,6 +29,11 @@ image_prevision = "images/prevision.png"
 image_zone = "images/zone.png"
 
 def normaliser(chaine):
+"""@summary: enlève les accents d'une chaîne de caractère et la met en minuscule. Remplace également les espaces et apostrophes par un underscore.
+	@chaine: chaîne de caractère à normaliser
+	@type: string
+	@return: La chaîne de caractère une fois les remplacements effectués.
+"""
 	chaine = chaine.lower()
 	chaine = chaine.replace(u" ",u"_")
 	chaine = chaine.replace(u"'",u"_")
